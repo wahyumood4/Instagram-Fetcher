0ef1ef3450600d3d28c57b0ec1e44dca97cd5641
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-09-06 13:22:42 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+const qs = require('querystring')
+const cheerio = require('cheerio');
+const imageToBase64 = require('image-to-base64');
+
+function download(link){
+var url = "https://sssinstagram.com/results";
+const requestBody = {
+id: link,
+}
+const config = {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+}
+axios.post(url, qs.stringify(requestBody), config)
+  .then((result) => {
+    // Do somthing
+let $ = cheerio.load(result.data);
+let ig = [];
+
+    $('div.button_div > a').each(function (i, e) {
+        ig[i] = $(this).attr("href");
+    });
+console.log(ig)
+return ig;
+}
+)        
+  .catch((err) => {
+console.log("sonething went wrong");
+  })
+}
+exports.download = download;
